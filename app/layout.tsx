7183874ac4c831ed9c387beb997a3fae145b8306
tetpic <rabustes@gmail.com>
2023-07-11 21:20:06 +0300

    init project, add some styles, fonts, variables

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import "./main.scss"
+
+export default function RootLayout({
+    children,
+  }: {
+    children: React.ReactNode
+  }) {
+    return (
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    )
+  }
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function MainPage() {
+    return(<>
+    <p>
+    Hello from next
+
+    </p>
+    </>)
+}
